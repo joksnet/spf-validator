8415d00b74ab3f9660edc10913f25068e325e155
Juan Martinez <juan.martinez@mediamonks.com>
2018-12-13 12:03:18 +0100

    Return empty list for DNS

--- test/main.test.js
+++ test/main.test.js
@@ -1,8 +1,23 @@
 var spf = require('../index');
+var dns = require('dns');
 
 test('Version is string', () => {
   expect(typeof spf.version).toBe('string');
 });
 test('SPFValidator is function', () => {
   expect(typeof spf.SPFValidator).toBe('function');
+});
+test('SPFValidator.getRecords returns empty array when no records has been found', () => {
+  let resolveTxt = jest.spyOn(dns, 'resolveTxt');
+  resolveTxt.mockImplementation((hostname, callback) => {
+    callback(null, []);
+  });
+
+  let validator = new spf.SPFValidator('example.com');
+  let promise = validator.getRecords('example.com').then(records => {
+    expect(records).toEqual([]);
+  });
+  expect(resolveTxt).toHaveBeenCalled();
+  resolveTxt.mockRestore();
+  return promise;
 });
